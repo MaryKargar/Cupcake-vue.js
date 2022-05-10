7a59ca586164fac3c3c7b9839a8d4d55ec6afe68
MaryKargar <92923078+MaryKargar@users.noreply.github.com>
2022-05-10 16:21:04 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <card-componnent :cakes='cakes'></card-componnent>
+  </div>
+</template>
+
+<script>
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CardComponnent from './components/CardComponnnent.vue'
+export default {
+  components:{
+    CardComponnent
+  },
+  data(){
+    return{
+      cakes:[
+        {title:'Chocolate CupCake', ingredients:'eggs, milk, oil, and vanilla', recipe:'Add the eggs, milk, oil, and vanilla. Mix on medium speed for about two minutes. Stir in boiling water. Dont worry if the batter seems a little thin, as reviewers say thats perfectly normal. ', img:'../assets/1.jpg' },
+        {title:'vanilla CupCake', ingredients:'eggs, milk, oil, and vanilla', recipe:'Add the eggs, milk, oil, and vanilla. Mix on medium speed for about two minutes. Stir in boiling water. Dont worry if the batter seems a little thin, as reviewers say thats perfectly normal. ',img:'../assets/ch.jfif' },
+        {title:'orange CupCake', ingredients:'eggs, milk, oil, and vanilla', recipe:'Add the eggs, milk, oil, and vanilla. Mix on medium speed for about two minutes. Stir in boiling water. Dont worry if the batter seems a little thin, as reviewers say thats perfectly normal. ',img:'../assets/ch.jfif' },
+        {title:'strawberry  CupCake', ingredients:'eggs, milk, oil, and vanilla', recipe:'Add the eggs, milk, oil, and vanilla. Mix on medium speed for about two minutes. Stir in boiling water. Dont worry if the batter seems a little thin, as reviewers say thats perfectly normal. ',img:'../assets/ch.jfif' },
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  align-items : center;
+  justify-content: center;
+}
+
+nav {
+  padding: 30px;
+  align-items : center;
+  justify-content: center;
+  
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  align-items : center;
+  justify-content: center;
+  
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+  align-items : center;
+  justify-content: center;
+  
+}
+</style>
---
+++ CardComponnnent.vue
@@ -0,0 +1,35 @@
+!<template>
+<div class="row">
+    <div v-for="(cake,index) in cakes" :key="index" class="card" style="width: 18rem;">
+  <img src="../assets/11.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{{cake.title}}</h5>
+    <p class="card-text">eggs, milk, oil, and vanilla</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Ingredients</li>
+    <li class="list-group-item">Topping</li>
+    <li class="list-group-item">Time</li>
+  </ul>
+  <div class="card-body">
+    <a href="https://www.gutekueche.at/schoko-cupcake-rezept-5032" class="card-link">www.gutekueche.at</a>
+    <a href="https://www.einfachbacken.de/rezepte/schoko-cupcakes" class="card-link">www.einfachbacken.de</a>
+  </div>
+</div>
+</div>
+  
+</template>
+
+<script>
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+props:['cakes']
+}
+</script>
+
+<style>
+cake {
+align: center;
+}
+</style>
